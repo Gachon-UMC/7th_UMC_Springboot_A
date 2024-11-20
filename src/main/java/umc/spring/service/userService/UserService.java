2d398ea5b8168554c2fad375b7a47ecfcf95f0a0
seungwon7934 <sw7934@gmail.com>
2024-11-21 01:04:00 +0900

    [feat] #4 Springboot 8주차 워크북 내용 수행 - 회원가입 API 구현 및 User Controller, Service, DTO, Converter 작성

---
+++ src/main/java/umc/spring/service/userService/UserService.java
@@ -0,0 +1,23 @@
+package umc.spring.service.userService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.spring.domain.user.Users;
+import umc.spring.dto.UserRegisterDto;
+import umc.spring.repository.UsersRepository;
+import umc.spring.converter.UsersConverter;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UsersRepository usersRepository;
+    private final UsersConverter usersConverter;
+
+    @Transactional
+    public Users createUser(UserRegisterDto request) {
+        Users user = usersConverter.toEntity(request);
+        return usersRepository.save(user);
+    }
+}
---
+++ src/main/java/umc/spring/web/controller/UserController.java
@@ -0,0 +1,23 @@
+package umc.spring.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import umc.spring.domain.user.Users;
+import umc.spring.dto.UserRegisterDto;
+import umc.spring.service.userService.UserService;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Users> createUser(@Validated @RequestBody UserRegisterDto request) {
+        Users createdUser = userService.createUser(request);
+        return ResponseEntity.ok(createdUser);
+    }
+}
